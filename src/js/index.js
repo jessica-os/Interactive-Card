e20855d1fd2f308062f0984ca7c0e78585cedd80
Jessica Oliveira <elisa0js@gmail.com>
2023-08-15 11:30:58 -0300

    refatoração js

--- src/js/index.js
+++ src/js/index.js
@@ -18,115 +18,116 @@ const confirmButton = document.querySelector("#confirm-button ");
 const thank = document.querySelector(".thank");
 const continueButton = document.querySelector(".continue-button");
 
-form.addEventListener("submit", event => {
-	event.preventDefault();
-	formValidate();
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  formValidate();
 });
 
 function formValidate() {
-	checkInputName();
-	checkInputNumber();
-	checkInputMonth();
-	checkInputYear();
-	checkInputCvc();
+  checkInputName();
+  checkInputNumber();
+  checkInputMonth();
+  checkInputYear();
+  checkInputCvc();
 
-	const inputItem = form.querySelectorAll(".input-item");
-	const valid = [...inputItem].every(item => {
-		return item.className === "input-item";
-	});
-	if (valid) {
-		form.style.display = "none";
-		thank.style.display = "block";
-	}
+  const inputItem = form.querySelectorAll(".input-item");
+  const valid = [...inputItem].every((item) => {
+    return item.className === "input-item";
+  });
+  if (valid) {
+    form.style.display = "none";
+    thank.style.display = "block";
+  }
 }
 continueButton.addEventListener("click", () => {
-	thank.style.display = "none";
-	form.style.display = "block";
+  thank.style.display = "none";
+  form.style.display = "block";
 });
 
 function error(input, message) {
-	const inputItem = input.parentElement;
-	const textMessage = inputItem.querySelector("a");
+  const inputItem = input.parentElement;
+  const textMessage = inputItem.querySelector("a");
 
-	textMessage.innerText = message;
-	inputItem.className = "input-item error";
+  textMessage.innerText = message;
+  inputItem.className = "input-item error";
 }
 
 function checkInputName() {
-	const inputNameValue = inputName.value;
-	if (inputNameValue === "") {
-		error(inputName, "can't be blank");
-	} else {
-		const inputItem = inputName.parentElement;
-		inputItem.className = "input-item";
-	}
+  const inputNameValue = inputName.value;
+  if (inputNameValue === "") {
+    error(inputName, "can't be blank");
+  } else {
+    const inputItem = inputName.parentElement;
+    inputItem.className = "input-item";
+  }
 }
 
 function checkInputNumber() {
-	const inputNumberValue = inputNumber.value;
-	if (inputNumberValue === "") {
-		error(inputNumber, "can't be blank");
-	} else if (inputNumberValue.length < 16) {
-		error(inputNumber, "Card number must be 16 numbers");
-	} else if (!/^\d+(\s\d+)*$/.test(inputNumberValue)) {
-		error(inputNumber, "Wrong format, numbers only");
-	} else {
-		const inputItem = inputNumber.parentElement;
-		inputItem.className = "input-item";
-	}
+  const inputNumberValue = inputNumber.value;
+
+  if (inputNumberValue === "") {
+    error(inputNumber, "can't be blank");
+  } else if (inputNumberValue.length < 16) {
+    error(inputNumber, "Card number must be 16 numbers");
+  } else if (!/^\d+(\s\d+)*$/.test(inputNumberValue)) {
+    error(inputNumber, "Wrong format, numbers only");
+  } else {
+    const inputItem = inputNumber.parentElement;
+    inputItem.className = "input-item";
+  }
 }
 
 function checkInputMonth() {
-	const inputMonthValue = inputMonth.value;
-	const inputItem = inputMonth.parentElement;
-	if (inputMonthValue === "") {
-		error(inputMonth, "can't be blank");
-	} else if (inputMonthValue.length < 2) {
-		error(inputMonth, "fill with 2 characters");
-	} else if (parseInt(inputMonthValue) > 12) {
-		error(inputMonth, "must not be greater than 12!");
-	} else {
-		const inputItem = inputMonth.parentElement;
-		inputItem.className = "input-item";
-	}
+  const inputMonthValue = inputMonth.value;
+  const inputItem = inputMonth.parentElement;
+  if (inputMonthValue === "") {
+    error(inputMonth, "can't be blank");
+  } else if (inputMonthValue.length < 2) {
+    error(inputMonth, "fill with 2 characters");
+  } else if (parseInt(inputMonthValue) > 12) {
+    error(inputMonth, "must not be greater than 12!");
+  } else {
+    const inputItem = inputMonth.parentElement;
+    inputItem.className = "input-item";
+  }
 }
 function checkInputYear() {
-	const inputYearValue = inputYear.value;
-	const inputItem = inputYear.parentElement;
-	if (inputYearValue === "" || inputYearValue.length < 2) {
-		inputItem.className = "input-item error";
-	} else {
-		const inputItem = inputYear.parentElement;
-		inputItem.className = "input-item";
-	}
+  const inputYearValue = inputYear.value;
+  const inputItem = inputYear.parentElement;
+  if (inputYearValue === "" || inputYearValue.length < 2) {
+    inputItem.className = "input-item error";
+  } else {
+    const inputItem = inputYear.parentElement;
+    inputItem.className = "input-item";
+  }
 }
 function checkInputCvc() {
-	const inputCvcValue = inputCvc.value;
-	const inputItem = inputCvc.parentElement;
-	if (inputCvcValue === "") {
-		error(inputCvc, "can't be blank");
-	} else if (inputCvcValue.length < 3) {
-		error(inputCvc, "fill with 3 characters");
-	} else {
-		const inputItem = inputCvc.parentElement;
-		inputItem.className = "input-item";
-	}
+  const inputCvcValue = inputCvc.value;
+  const inputItem = inputCvc.parentElement;
+  if (inputCvcValue === "") {
+    error(inputCvc, "can't be blank");
+  } else if (inputCvcValue.length < 3) {
+    error(inputCvc, "fill with 3 characters");
+  } else {
+    const inputItem = inputCvc.parentElement;
+    inputItem.className = "input-item";
+  }
 }
 
 inputName.addEventListener("input", () => {
-	cardName.innerText = inputName.value;
+  cardName.innerText = inputName.value;
 });
 
 inputNumber.addEventListener("input", () => {
-	cardNumber.innerText = inputNumber.value;
+  cardNumber.innerText = inputNumber.value.match(/.{1,4}/g).join(" ");
 });
 
 inputCvc.addEventListener("input", () => {
-	cardCvc.innerText = inputCvc.value;
+  cardCvc.innerText = inputCvc.value;
 });
 inputMonth.addEventListener("input", () => {
-	cardMonth.innerText = inputMonth.value;
+  cardMonth.innerText = inputMonth.value;
 });
 inputYear.addEventListener("input", () => {
-	cardYear.innerText = inputYear.value;
+  cardYear.innerText = inputYear.value;
 });
